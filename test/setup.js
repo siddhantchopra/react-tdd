ee5dcf0205d1dc1459121fa5c3fbe67cad0971a1
siddhantchopra <siddanthchopra@gmail.com>
2020-10-24 13:39:42 +0530

    updated code: Fifth commit
    updated setup.js

--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,4 @@
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-jest.mock('../src/idgen', () => jest.fn(() => 'mockId'));
-global.M = require('materialize-css');
+global.M = require('materialize-css'); // here was error during unit test M is not defined
 Enzyme.configure({adapter: new Adapter()})
